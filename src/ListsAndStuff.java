33ceb2a9b21c26ceb4e32ba8db643c074090ca63
parrwgrah <parrgraham888@gmail.com>
2024-12-24 15:32:31 +1300

    Todays work on figuring out the Brick Program

--- src/ListsAndStuff.java
+++ src/ListsAndStuff.java
@@ -1,3 +1,6 @@
+import ecs100.UI;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,5 +18,22 @@ public class ListsAndStuff {
         //def return to this
         return new ArrayList<>(Arrays.asList(arr));
     }
+
+    boolean isRunning = false;
+    String buttonLabel = "start";
+
+    void setupGUI(){
+        UI.addButton(buttonLabel, this::startOrStop);
+    }
+    void startOrStop(){
+        if (isRunning) {
+            buttonLabel = "start";
+        } else {
+            buttonLabel = "stop";
+        }
+        isRunning = !isRunning;
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
 import java.util.ArrayList;
+import ecs100.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        // runtime instance associated with
+        // the current Java application
+//        Runtime runtime = Runtime.getRuntime();
+//
+//        // Enable instruction tracing
+//        runtime.traceInstructions(true);
+
         ArrayList<String> list1;
         String[] arr1 = {"a", "b", "c", "d", "e", "f", "g", "h"};
         ListsAndStuff listsAndStuff = new ListsAndStuff();
         list1 = listsAndStuff.makeBasicLi(arr1);
         System.out.println(list1.size());
+
+
+
+        listsAndStuff.setupGUI();
+
     }
+
+
 }
